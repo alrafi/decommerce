3c9ce31e0ef8e47d18b533ba0811e6f84d0656f1
alrafi <hafisalrafi@gmail.com>
2020-12-01 17:55:55 +0700

    add route to app

--- src/App.js
+++ src/App.js
@@ -1,39 +1,15 @@
-import React, {useEffect, useState} from 'react';
-import store from './api/store'
+import React from 'react';
+import './assets/styles/index.scss'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home/Home';
 
 const App = () => {
-  const [products, setProducts] = useState([])
-
-  useEffect(() => {
-    const getProducts = async () => {
-      try {
-        const res = await store.get('/products');
-        console.log(res.data)
-        setProducts(res.data);
-      } catch (err) {
-        console.log(err);
-        return;
-      }
-    };
-    getProducts();
-  }, [])
-
-  if (!products) return;
-
   return (
-    <div>
-      <ul>
-        {
-          products.map(item => {
-            return (
-              <li key={item.id}>
-                {item.title}
-              </li>
-            )
-          })
-        }
-      </ul>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" component={Home} exact />
+      </Switch>
+    </Router>
   );
 };
 
